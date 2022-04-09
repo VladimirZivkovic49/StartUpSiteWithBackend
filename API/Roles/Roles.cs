39efd802d0fadeb2a3bf110a8186fa748986e675
SIMCON-VM\Creen <Milo Stankovi>
2022-04-09 15:20:03 +0200

    Dodate role

---
+++ API/Roles/Roles.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Roles
+{
+    public class Roles
+    {
+        public string Admin{ get; set; }
+        public string RegistredUser{ get; set; }
+
+        public string CommonUser { get; set; }
+    }
+}
