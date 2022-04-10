ab9e17bfb6428a6ae9ba3aca7c62755d6334706c
VladTest11 <markovicvladimirr@gmail.com>
2022-04-10 19:13:04 +0200

    dodata komponenta AboutCompany; postavljena Home komponenta da bude landing page

--- startupfront/src/app/app-routing.module.ts
+++ startupfront/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { TeamMembersComponent } from './teamInHouse/team-members/team-members.component';
@@ -7,6 +8,7 @@ import { TeamMembersComponent } from './teamInHouse/team-members/team-members.co
 const routes: Routes = 
 [
   {path:'',component:HomeComponent},
+  {path:'app-about-company',component:AboutCompanyComponent},
   {path:'contact',component:ContactComponent},
   { path:'teamMembers', component:TeamMembersComponent },
   {path:'**',component:HomeComponent,pathMatch:'full'}
